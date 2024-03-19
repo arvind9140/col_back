fcfcaa671a02d0c3884d0866e5888c53d4d64a7a
Arvind Maurya <ar789maurya@gmail.com>
2024-03-19 11:36:52 +0530

    check logout

--- controllers/usersControllers/logout.controller.js
+++ controllers/usersControllers/logout.controller.js
@@ -5,32 +5,41 @@ export const logout = async (req, res) => {
   const userId = req.body.userId;
   const token = req.body.token;
 
-  await loginModel
-    .find({ userID: userId })
-    .exec()
-    .then((result) => {
-      if (result.length < 1) {
-        responseData(res, "", 401, false, "User not exist");
-      }
-      if (result.length > 0) {
-        let filterData = result.filter((d) => {
-          return d.token == req.body.token;
-        });
-        loginModel.findByIdAndUpdate(
-          filterData[0]._id,
-          { token: " " },
-          (err, res) => {
-            if (err) {
-              ////// Here add the respnse for error
-              console.log(err);
-            } else {
-              console.log("logout successfull");
+  try {
+    await loginModel
+      .find({ userID: userId })
+      .exec()
+      .then((result) => {
+        if (result.length < 1) {
+          responseData(res, "", 401, false, "User not exist");
+        }
+        if (result.length > 0) {
+          let filterData = result.filter((d) => {
+            return d.token == token;
+          });
+          loginModel.findByIdAndUpdate(
+            filterData[0]._id,
+            { token: " " },
+            (err, res) => {
+              if (err) {
+                ////// Here add the respnse for error
+                console.log(err);
+              } else {
+                console.log("logout successfull");
+              }
             }
-          }
-        );
+          );
 
-        res.clearCookie("auth");
-        responseData(res, "Logout successfully", 200, true, "");
-      }
-    });
+          res.clearCookie("auth");
+          responseData(res, "Logout successfully", 200, true, "");
+        }
+      });
+
+  }
+  catch (err) {
+    console.log(err);
+    responseData(res, "", 500, false, "Internal Server Error");
+  }
+
+
 };
