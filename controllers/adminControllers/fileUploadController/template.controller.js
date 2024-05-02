3efaacb5c6bff567f7dd252091fdfe215c3e5bdc
Arvind Maurya <ar789maurya@gmail.com>
2024-05-02 10:02:01 +0530

    set foldername and sub folder name first in template data

--- controllers/adminControllers/fileUploadController/template.controller.js
+++ controllers/adminControllers/fileUploadController/template.controller.js
@@ -158,8 +158,8 @@ export const templateFileUpload = async (req, res) => {
             }
            
 
-            if (folder_name === "commercial" || folder_name === "residential" || folder_name === "miscellaneous") {
-                if (sub_folder_name_first === "designing" || sub_folder_name_first === "executing" || sub_folder_name_first === "miscellaneous") {
+            if (folder_name === "commercial" || folder_name === "residential" || folder_name === "company data") {
+                if (sub_folder_name_first === "designing" || sub_folder_name_first === "executing" || sub_folder_name_first === "company policies") {
                     const folder_Id = `FOL_ID${generateSixDigitNumber()}`;
                     const check_type = await fileuploadModel.findOne({
                         type: type,
---
+++ controllers/usersControllers/user.profile.update.controller.js
@@ -0,0 +1,7 @@
+import registerModel from "../../models/usersModels/register.model.js";
+
+
+export const updateUserProfile = async(req,res) =>{
+const userId = req.body.userId;
+
+}
